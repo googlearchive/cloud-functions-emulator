d0bc5cd36b505dcedbf5a6215e40ed049f4dbe84
Tina Liang <tinaliang@google.com>
2018-02-28 11:24:42 -0800

    cleaned up code

--- src/supervisor/worker.js
+++ src/supervisor/worker.js
@@ -74,7 +74,7 @@ function main () {
 
     // Require the target module to load the function for invocation
     const functionModule = require(localdir);
-    const handler = _.get(functionModule, cloudfunction.entryPoint, functionModule[name]);
+    const handler = _.get(functionModule, cloudfunction.entryPoint || name);
 
     if (!handler) {
       throw new Error(`No function found with name ${cloudfunction.entryPoint || name}`);
